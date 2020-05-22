81837bc495eb698a7a0f51e00be56956617ceac8
rlee21 <rlee21@comcast.net>
2020-05-22 15:56:37 -0700

    phase 1 update

--- Gemfile
+++ Gemfile
@@ -20,6 +20,13 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+
+# Flexible authentication solution for Rails
+gem 'devise', '~> 4.7', '>= 4.7.1'
+
+gem 'bootstrap-sass', '3.3.7'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
---
+++ app/assets/javascripts/productModal.js
@@ -0,0 +1,30 @@
+var modal = document.querySelector('#product-modal');
+var modalContent = document.querySelector('.close');
+var productDescription = document.querySelector('.product-description');
+
+function showModal(product) {
+  modal.style.display = 'block';
+  updateProductDescription(product);
+}
+
+function updateProductDescription(product) {
+  productDescription.innerHTML = `
+    ${product.status}<br>
+    Name: ${product.name}<br>
+    Description: ${product.description}<br>
+    Price: ${product.price}<br>
+    <form class="add-to-cart" action="/users">
+    <input type="submit" value="Add to Cart" />
+    </form>
+  `;
+}
+function removeModal() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
