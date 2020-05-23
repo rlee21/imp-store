039f6018ef925dd9bdc807eb9d67b3a64b1ecdf3
rlee21 <rlee21@comcast.net>
2020-05-23 09:02:31 -0700

    add search functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def search
+    @results = Product.where("lower(name) LIKE ?", "%#{params[:search].downcase}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "products#index"
   devise_for :users
-  resources :products
+  resources :products do
+    collection { get :search }
+  end
   get '/about', to: 'static_pages#about'
 end
